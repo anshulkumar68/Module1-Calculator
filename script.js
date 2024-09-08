75fbf1df71df1339ba87b02a4c8770b4675cc802
Anshul <anshul3071@gmail.com>
2024-09-08 23:40:16 +0530

    started building calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+function showTextContent() {
+  let element = document.getElementById("example");
+  alert("textContent: " + element.textContent);
+}
+
+function showInnerText() {
+  let element = document.getElementById("example");
+  alert("innerText: " + element.innerText);
+}
