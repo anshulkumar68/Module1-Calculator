d15d2eeef4781a34e5769fc94f74b6c0e95fef41
Groot code <anshul3071@gmail.com>
2024-09-13 12:27:35 +0530

    updated calc app

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 'use strict';
 
+// // using eval() function
+// //   function calculate(){
+// //     try{
+// //       let result = eval(display.value);
+// //       if(result%1 !==0)
+// //         display.value = result.toFixed(3);
+// //       else
+// //         display.value = result;
+// //     }
+// //     catch(error){
+// //       console.log(error);
+// //       display.value = "error";
+// //     }
+// //   }
 
 // Select the display input field
 const display = document.querySelector('.input-field');
@@ -19,92 +33,80 @@ function handleDelete() {
   display.value = display.value.toString().slice(0, -1);
 }
 
-function calculate(){
-    try{
-        let tokens = tokenize(display.value);
-        let result = evaluateExpression(tokens);
-        display.value = result;
-    }
-    catch(error){
-        display.value = 'error';
-    }
-}  
-
-function tokenize(expression){
-    const regex = /\d+|\S/g;
-    return expression.match(regex);
+// Function to calculate the result
+function calculate() {
+  try {
+    const tokens = tokenize(display.value);
+    const result = evaluateExpression(tokens);
+    display.value = result;
+  } catch (error) {
+    display.value = 'error';
+  }
 }
 
-function evaluateExpression(tokens){
-    const operandStack = [];
-    const operatorStack = [];
-    const precedence = {
-        '+' : 1,
-        '-' : 1,
-        '*' : 2,
-        '/' : 2,
-    }
+// Tokenize the input string into numbers and operators
+function tokenize(expression) {
+  const regex = /\d+|\S/g;
+  return expression.match(regex);
+}
 
-    // process each token
-    tokens.forEach(token => {
-        if(!isNaN(token)){
-            operandStack.push(parseFloat(token));
-        } else if(['+', '-', '*', '/'].includes(token)){
-            while(operatorStack.length && precedence[operatorStack[operatorStack.length - 1]] >= precedene[token]){
-                processOperator();
-            }
-            operatorStack.push(token);
-        }
-    });
+// Evaluate the expression using stacks
+function evaluateExpression(tokens) {
+  const operandStack = []; // Stack for numbers
+  const operatorStack = []; // Stack for operators
 
-    // Process remaining operators
-    while(operatorStack.length){
+  const precedence = {
+    '+': 1,
+    '-': 1,
+    '*': 2,
+    '/': 2,
+  };
+
+  // Process each token
+  tokens.forEach(token => {
+    if (!isNaN(token)) {
+      operandStack.push(parseFloat(token));
+    } else if (['+', '-', '*', '/'].includes(token)) {
+      while (operatorStack.length &&
+             precedence[operatorStack[operatorStack.length - 1]] >= precedence[token]) {
         processOperator();
+      }
+      operatorStack.push(token);
     }
+  });
 
-    //Function to apply an operator to operands
-    function processOperator(){
-        const operator = operatorStack.pop();
-        const rightOperand = operandStack.pop();
-        const leftOperand = operandStack.pop();
+  // Process remaining operators
+  while (operatorStack.length) {
+    processOperator();
+  }
 
-        let result;
-        switch(operator){
-            case '+':
-                result = leftOperand + rightOperand;
-                break;
-            case '-':
-                result = leftOperand - rightOperand;
-                break;
-            case '*':
-                result = leftOperand * rightOperand;
-                break;
-            case '/':
-                if(rightOperand === 0){
-                    throw new Error("Division by zero");
-                }
-                result = leftOperand / rightOperand;
-                break;
+  // Function to apply an operator to operands
+  function processOperator() {
+    const operator = operatorStack.pop();
+    const rightOperand = operandStack.pop();
+    const leftOperand = operandStack.pop();
+
+    let result;
+    switch (operator) {
+      case '+':
+        result = leftOperand + rightOperand;
+        break;
+      case '-':
+        result = leftOperand - rightOperand;
+        break;
+      case '*':
+        result = leftOperand * rightOperand;
+        break;
+      case '/':
+        if (rightOperand === 0) {
+          throw new Error("Division by zero");
         }
-        operandStack.push(result);
+        result = leftOperand / rightOperand;
+        break;
     }
-    // the result should be the only item on the operand stack
-    return operandStack.pop();
+    operandStack.push(result);
+  }
+
+  // The result should be the only item on the operand stack
+  return operandStack.pop();
 }
-
-
-// using eval() function
-//   function calculate(){
-//     try{
-//       let result = eval(display.value);
-//       if(result%1 !==0)
-//         display.value = result.toFixed(3);
-//       else
-//         display.value = result;
-//     }
-//     catch(error){
-//       console.log(error);
-//       display.value = "error";
-//     }
-//   }
-
